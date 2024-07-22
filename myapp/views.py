302e258d981a7798ac28f34efc06c78a00c0b55c
mantavya0807 <mantavyamahajan@gmail.com>
2024-07-22 23:39:25 +0530

    deploy

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,8 @@ def index(request):
   # No data needed for this view, so we can pass an empty dictionary
   context = {}
   return render(request, 'index.html', context)
+
+def project(request):
+  # No data needed for this view, so we can pass an empty dictionary
+  context = {}
+  return render(request, 'project.html', context)
